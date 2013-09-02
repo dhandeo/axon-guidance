ae1ee76657464b8e1370e0677217f16f87633e9a
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-09-02 19:59:47 -0400

    Cleanup on the styles

--- js/AxonGrowthAnimation.js
+++ js/AxonGrowthAnimation.js
@@ -100,8 +100,6 @@ function AxonGrowthAnimation(config) {
             }); // END OpenLayers.Map
 
         // allow testing of specific renderers via "?renderer=Canvas", etc
-        var renderer = OpenLayers.Util.getParameters(window.location.href).renderer;
-        renderer = (renderer) ? [renderer] : OpenLayers.Layer.Vector.prototype.renderers;
 
         /*
          * Layer style
@@ -117,48 +115,26 @@ function AxonGrowthAnimation(config) {
         var style_blue = OpenLayers.Util.extend({}, layer_style);
         style_blue.strokeColor = "blue";
         style_blue.fillColor = "blue";
-        //style_blue.graphicName = "star";
         style_blue.pointRadius = 5;
         style_blue.strokeWidth = 2;
-        style_blue.strokeLinecap = "butt";
+        style_blue.strokeLinecap = "solid";
         style_blue.title = "0,0";
 
         /*
          * Green style
          */
         var style_green = {
-            strokeColor: "#00FF00",
-            strokeWidth: 3,
-            strokeDashstyle: "dashdot",
-            pointRadius: 6,
-            pointerEvents: "visiblePainted",
-            title: "this is a green line"
+            strokeColor: "green",
+            fillColor: "green",
+            pointRadius: 5,
+            strokeWidth: 4,
+            strokeDashstyle: "solid",
+            title: "50,50"
         };
-
-        /*
-         * Mark style
-         */
-        var style_mark = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
-        // each of the three lines below means the same, if only one of
-        // them is active: the image will have a size of 24px, and the
-        // aspect ratio will be kept
-        // style_mark.pointRadius = 12;
-        // style_mark.graphicHeight = 24;
-        // style_mark.graphicWidth = 24;
-
-        // if graphicWidth and graphicHeight are both set, the aspect ratio
-        // of the image will be ignored
-        style_mark.graphicWidth = 24;
-        style_mark.graphicHeight = 20;
-        style_mark.graphicXOffset = 10; // default is -(style_mark.graphicWidth/2);
-        style_mark.graphicYOffset = -style_mark.graphicHeight;
-        style_mark.externalGraphic = "../img/marker.png";
-        // title only works in Firefox and Internet Explorer
-        style_mark.title = "this is a test tooltip";
 
         var vectorLayer = new OpenLayers.Layer.Vector("Simple Geometry", {
             style: layer_style,
-            renderers: renderer,
+            renderers: ["SVG", "SVG"],
             isBaseLayer: true
         });
 
@@ -167,34 +143,6 @@ function AxonGrowthAnimation(config) {
         var pointFeature = new OpenLayers.Feature.Vector(point,null,style_blue);
         var point2 = new OpenLayers.Geometry.Point(50, 50);
         var pointFeature2 = new OpenLayers.Feature.Vector(point2,null,style_green);
-//        var point3 = new OpenLayers.Geometry.Point(-50, 50);
-//        var pointFeature3 = new OpenLayers.Feature.Vector(point3,null,style_mark);
-
-        // create a line feature from a list of points
-//        var pointList = [];
-//        var newPoint = point;
-//        for(var p=0; p<15; ++p) {
-//            newPoint = new OpenLayers.Geometry.Point(newPoint.x + Math.random(1),
-//                                                     newPoint.y + Math.random(1));
-//            pointList.push(newPoint);
-//        }
-//        var lineFeature = new OpenLayers.Feature.Vector(
-//            new OpenLayers.Geometry.LineString(pointList),null,style_green);
-//
-//        // create a polygon feature from a linear ring of points
-//        var pointList = [];
-//        for(var p=0; p<6; ++p) {
-//            var a = p * (2 * Math.PI) / 7;
-//            var r = Math.random(1) + 1;
-//            var newPoint = new OpenLayers.Geometry.Point(point.x + (r * Math.cos(a)),
-//                                                         point.y + (r * Math.sin(a)));
-//            pointList.push(newPoint);
-//        }
-//        pointList.push(pointList[0]);
-
-//        var linearRing = new OpenLayers.Geometry.LinearRing(pointList);
-//        var polygonFeature = new OpenLayers.Feature.Vector(
-//            new OpenLayers.Geometry.Polygon([linearRing]));
 
 
         that.map.addLayer(vectorLayer);
@@ -202,13 +150,13 @@ function AxonGrowthAnimation(config) {
         vectorLayer.addFeatures([pointFeature, pointFeature2])
 
         that.vectorLayer = vectorLayer;
-        that.style_green = style_blue;
+        that.style_green = style_green;
+        that.style_blue = style_blue;
+
     };
     Init();
 
-    AddProtrusion(0,55);
-    AddProtrusion(90,16);
-    AddProtrusion(45,25);
-
-
+//    AddProtrusion(0,55);
+//    AddProtrusion(90,16);
+//    AddProtrusion(45,25);
 }
